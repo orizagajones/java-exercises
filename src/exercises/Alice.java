4fea0fb0d41a6d9740e0eecd0959547db9c54ba0
Rhiannon Orizaga <orizaga.jones@gmail.com>
2017-07-31 20:42:09 -0700

    all my changes to Alice and Area

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -8,14 +8,19 @@ public class Alice {
     public static void main(String[] args) {
         String firstSentence = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
         String searchTerm;
-        Scanner in;
+        Scanner input;
 
-        in = new Scanner(System.in);
+        input = new Scanner(System.in);
         System.out.println(firstSentence);
-        System.out.println("Please enter a word to search for in the sentence.");
-        searchTerm = next();
+        System.out.println("Please enter a word to search for in the sentence:");
+        searchTerm = input.next();
 
-
+        if (firstSentence.toLowerCase().contains(searchTerm = searchTerm.toLowerCase())) {
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+// one problem with doing it this way is that a non-word like 'lice' works, too
 
     }
 }
--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -1,8 +1,33 @@
+package org.launchcode.java.studios;
+
 import java.util.Scanner;
 
+import static java.lang.Math.PI;
+
 public class Area {
+
+    public static float calcArea(float radius) {
+        return (float) (Math.PI * Math.pow(radius, 2));
+        // return (float) (3.14 * radius * radius);
+    }
     //write a program that will calculate the area of a circle based on user input
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); //System.in means it will be input from your console
+
+        System.out.println("Please enter the radius of the circle: ");
+        float userRadius = input.nextFloat();
+        while(userRadius <= 0) {
+            System.out.println("Please enter a valid radius.");
+            userRadius = input.nextFloat();
+        }
+
+        float userArea = calcArea(userRadius);
+        System.out.printf("Your area is %f for radius of %f. Have a nice day.", userArea, userRadius);
+
+    }
+}
+
+/* Here is how I initially wrote it:
         Double radius;
         Double area;
         Scanner in;
@@ -12,6 +37,13 @@ public class Area {
         radius = in.nextDouble();
         area = (radius * radius) * 3.14;
 
-        System.out.println("The area of the circle with radius " + radius + "is " + area);
-    }
-}
+        System.out.println("The area of the circle with radius " + radius + "is " + area); */
+
+/* you can also use a do/while:
+do {
+    userRadius = input.nextFloat();
+    if (userRadius <= 0)
+        System.out.println("Invalid radius... please try again."
+    }while(userRadius <= 0);
+
+ */
